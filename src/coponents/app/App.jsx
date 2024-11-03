63dd21b6cb4b676030c29d4d8a8565e25d4767b3
Xenia <andreeva19-91@mail.ru>
2024-11-03 21:30:48 +0300

    firat configs

---
+++ src/coponents/app/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Hello, aviasales</h1>
+            <div>Here is going to be your application</div>
+        </div>
+    )
+}
+
+export default App;
