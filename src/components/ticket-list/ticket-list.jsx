9d186697101529ce37e503bbde489e7624c40478
Xenia <andreeva19-91@mail.ru>
2024-11-15 03:58:36 +0300

    feat: add show Tuckets while loading

--- src/components/ticket-list/ticket-list.jsx
+++ src/components/ticket-list/ticket-list.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { nanoid } from 'nanoid';
 
@@ -8,9 +8,11 @@ import { fetchTicketsThunk, fetchSearchId, ticketSlice } from '../../redux/reduc
 import classes from './ticket-list.module.scss';
 
 const TicketList = () => {
+  const [isSliced, setIsSliced] = useState(false);
   const dispatch = useDispatch();
   const tickets = useSelector((state) => state.tickets.tickets);
   const stop = useSelector((state) => state.tickets.stop);
+  const isLoading = useSelector((state) => state.tickets.isLoading);
   const { searchId } = useSelector((state) => state.searchId);
   const shownTickets = useSelector((state) => state.tickets.shownTickets);
   useEffect(() => {
@@ -22,10 +24,11 @@ const TicketList = () => {
     }
   }, [searchId, tickets, dispatch, stop]);
   useEffect(() => {
-    if (stop) {
+    if (tickets.length > 0 && !isSliced) {
       dispatch(ticketSlice.actions.sliceTickets());
+      setIsSliced(true);
     }
-  }, [stop, dispatch]);
+  }, [tickets]);
   let ticketList = [];
   if (shownTickets.length > 0) {
     ticketList = shownTickets.map((ticket) => {
